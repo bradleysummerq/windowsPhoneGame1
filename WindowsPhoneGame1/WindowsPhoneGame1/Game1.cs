c679f8f810bfcb34c60b2e7da53c8170bcead293
unknown <Brad@.redmond.corp.microsoft.com>
2010-08-06 15:06:36 -0700

    simple text scrolling examples

--- WindowsPhoneGame1/WindowsPhoneGame1/Game1.cs
+++ WindowsPhoneGame1/WindowsPhoneGame1/Game1.cs
@@ -30,7 +30,18 @@ namespace WindowsPhoneGame1
         int sprite2Height;
         int sprite2Width;
 
-        //SoundEffect soundEffect;
+        const float SPEED = 240f / 1000; // pixels per millisecond
+        const string TEXT = "Hello, Windows Phone!";
+ 
+        SpriteFont kootenay14;
+        Vector2 midPoint;
+        Vector2 pathVector;
+        Vector2 pathDirection;
+        Vector2 textPosition;
+        Vector2 position1;
+        float lapSpeed;
+        float tLap;
+        SoundEffect soundEffect;
 
 
         public Game1()
@@ -68,7 +79,22 @@ namespace WindowsPhoneGame1
             texture1 = Content.Load<Texture2D>("GameThumbnail");
             texture2 = Content.Load<Texture2D>("GameThumbnail");
 
-           // soundEffect = Content.Load<SoundEffect>("crap");
+            soundEffect = Content.Load<SoundEffect>("explosion");
+
+
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            Rectangle clientBounds = this.Window.ClientBounds;
+            kootenay14 = this.Content.Load<SpriteFont>("Kootenay14");
+            Vector2 textSize = kootenay14.MeasureString(TEXT);
+            position1 = new Vector2(clientBounds.Right - textSize.X, clientBounds.Top);
+            Vector2 position2 = new Vector2(clientBounds.Left, clientBounds.Bottom - textSize.Y);
+            midPoint = Vector2.Lerp(position1, position2, 0.5f);
+            pathVector = position2 - position1;
+            lapSpeed = SPEED / (2 * pathVector.Length());
+            pathDirection = pathVector;
+            pathDirection.Normalize();
+            textPosition = position1;
+
 
             spritePosition1.X = 0;
             spritePosition1.Y = 0;
@@ -105,6 +131,31 @@ namespace WindowsPhoneGame1
             CheckForCollision();
 
             base.Update(gameTime);
+
+
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+            {
+                this.Exit();
+            }
+
+            float pixelChange = SPEED * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            textPosition += pixelChange * pathDirection;
+            if ((textPosition - midPoint).LengthSquared() > (0.5f * pathVector).LengthSquared())
+            {
+                float excess = (textPosition - midPoint).Length() -
+                    (0.5f * pathVector).Length();
+
+                pathDirection = -pathDirection;
+                textPosition += 2 * excess * pathDirection;
+            }
+            tLap += lapSpeed * (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+            tLap %= 1;
+            float pLap = tLap < 0.5f ? 2 * tLap : 2 - 2 * tLap;
+            textPosition = position1 + pLap * pathVector;
+
+            base.Update(gameTime);
+
         }
 
         void UpdateSprite(GameTime gameTime, ref Vector2 spritePosition, ref Vector2 spriteSpeed)
@@ -155,11 +206,10 @@ namespace WindowsPhoneGame1
 
             if (bb1.Intersects(bb2))
             {
-                //soundEffect.Play();
+                soundEffect.Play();
             }
 
         }
-
 
         protected override void Draw(GameTime gameTime)
         {
@@ -168,15 +218,15 @@ namespace WindowsPhoneGame1
             // Draw the sprite.
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.AlphaBlend);
             spriteBatch.Draw(texture1, spritePosition1, Color.White);
+            spriteBatch.DrawString(kootenay14, TEXT, textPosition, Color.White);
             spriteBatch.End();
+
 
             spriteBatch.Begin(SpriteSortMode.BackToFront, BlendState.Opaque);
             spriteBatch.Draw(texture2, spritePosition2, Color.Gray);
             spriteBatch.End();
 
             base.Draw(gameTime);
-
         }
-
     }
 }
